da8a52a99c5a919162c95af074cbb3b695e60fb1
Jmcclary27 <jadenmcclary@gmail.com>
2025-12-02 15:16:09 -0600

    Lesson 1

--- lessons/lesson01_execution_model/print_demo.cu
+++ lessons/lesson01_execution_model/print_demo.cu
@@ -1,12 +1,27 @@
+#include <cstdio>
 #include <cuda_runtime.h>
-#include <stdio.h>
 
-extern "C" __global__
-void print_thread_mapping(int n) {
+__global__ void print_thread_mapping(int n) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
     if (idx < n) {
         printf("block %d, thread %d --> global index %d\n",
                blockIdx.x, threadIdx.x, idx);
     }
+}
+
+int main() {
+    int n = 16;
+    int blockSize = 4;
+    int gridSize  = (n + blockSize - 1) / blockSize;
+
+    printf("print_thread_mapping demo\n");
+    printf("  n         = %d\n", n);
+    printf("  blockSize = %d threads per block\n", blockSize);
+    printf("  gridSize  = %d blocks\n\n", gridSize);
+
+    print_thread_mapping<<<gridSize, blockSize>>>(n);
+    cudaDeviceSynchronize();
+
+    return 0;
 }
